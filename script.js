d5681fc204f69e6998afea9262f7c5c641026b4d
tyler.maran <tyler.maran@gmail.com>
2019-02-16 11:03:49 -0800

    adding nav bar and trucking/dumpster page

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('hello world');
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    var lexington = {
+        lat: 38.040098,
+        lng: -84.504511
+    };
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 9,
+            center: lexington
+        });
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({
+        position: lexington,
+        map: map
+    });
+}
